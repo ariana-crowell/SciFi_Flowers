4b5d30c0c28104e475ababd28b50d362ef368183
Kaitlyn-kodes <Kaitlynellis29@gmail.com>
2024-10-17 07:51:23 -0500

    Contact page contact form

--- contact.php
+++ contact.php
@@ -11,6 +11,30 @@
             require "includes/header.php"; 
         ?> 
     <center><h1>Contact Us!</h1></center>
+    <div class="container">
+      <form action="action_page.php">
+
+        <label for="fname">First Name</label>
+        <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+        <label for="lname">Last Name</label>
+        <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+        <label for="lname">Phone Number</label>
+        <input type="text" id="lname" name="lastname" placeholder="Your phone number..">
+
+        <label for="lname">Email</label>
+        <input type="text" id="lname" name="lastname" placeholder="Your email..">
+
+        
+
+        <label for="subject">Subject</label>
+        <textarea id="message" name="subject" placeholder="..." style="height:200px"></textarea>
+
+        <input type="submit" value="Submit">
+
+      </form>
+    </div>
     
 
 
