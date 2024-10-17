dc1058ee4ffc22710c4b0095979b42a0fdf2393b
LOV3NIAA <155994803+LOV3NIAA@users.noreply.github.com>
2024-10-17 08:44:21 -0500

    update

--- index.php
+++ index.php
@@ -24,6 +24,48 @@
                     ?>        
                 </section> 
             </div> 
+             <!-- Carousel container -->
+<div class="carousel">
+
+<!-- Full-width images with number and caption text -->
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="C:\Users\Ania.Ross\Downloads\fly trap.jfif" style="width:100%">
+  <div class="text">Caption Text</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="C:\Users\Ania.Ross\Downloads\squigily flower.jfif" style="width:100%">
+  <div class="text">Caption Two</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="C:\Users\Ania.Ross\Downloads\Lily-of-the-valley-flowers.webp" style="width:100%">
+  <div class="text">Caption Three</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="https://th.bing.com/th/id/OIP.5k7qM_aJ3QJ5ogAKomkhEgHaDj?w=317&h=168&c=7&r=0&o=5&pid=1.7" style="width:100%">
+  <div class="text">Caption Four</div>
+</div>
+
+
+<div class="mySlides fade">
+  <div class="https://th.bing.com/th/id/OIP.A6LOpP3xJ1iH3OAw6bl2KAHaE7?w=295&h=197&c=7&r=0&o=5&pid=1.7" style="width:100%">
+  <div class="text">Caption Five</div>
+</div>
+
+<!-- Next and previous buttons -->
+<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+<a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+
+</div>
         </main> 
     </body>
 </html>
