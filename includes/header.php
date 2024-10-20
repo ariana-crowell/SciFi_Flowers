0f2d625f13679e17a853a3ed0bcdac0b8798dff0
ariana-crowell <crowella66@gmail.com>
2024-10-19 20:40:59 -0500

    Update header.php
    restyled the signup link to match the rest of the words on all the other pages

--- includes/header.php
+++ includes/header.php
@@ -10,6 +10,12 @@
         <title>Document</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="style.css">
+        <style>
+            .signup {
+                font-family: Jacques Francois Shadow;
+                color:whitesmoke;
+            }
+        </style>
 </head>
     <body>
             <nav class="navbar navbar-expand-lg">
@@ -38,7 +44,7 @@
                             <input type="password" name="pwd" placeholder="Password..."> 
                             <button type="submit" name="login-submit">Login</button> 
                             </form> 
-                            <a href="signup.php">Signup</a>'; 
+                            <a class="signup" href="signup.php">Signup</a>'; 
                         }
                     ?>
                     <section> 
