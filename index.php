b3023e5e17ef8543cb1db4ee99fbf756e1657e05
LOV3NIAA <155994803+LOV3NIAA@users.noreply.github.com>
2024-10-22 08:26:59 -0500

    r

--- index.php
+++ index.php
@@ -24,28 +24,33 @@
                     ?>        
                 </section> 
 
-                <div id="carouselExampleFade" class="carousel slide carousel-fade">
+
+<div id="carouselExampleIndicators" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="C:\Users\Ania.Ross\Downloads\fly trap.jfi" class="d-block w-100" alt="...">
+      <img src="C:\xampp\htdocs\SciFi_Flowers\img\fly trap.jfif" class="d-block w-100">
     </div>
     <div class="carousel-item">
-      <img src="C:\Users\Ania.Ross\Downloads\squigily flower.jfif" class="d-block w-100" alt="...">
+      <img src="C:\xampp\htdocs\SciFi_Flowers\img\squigily flower.jfif" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="C:\Users\Ania.Ross\Downloads\Lily-of-the-valley-flowers.webp" class="d-block w-100" alt="...">
+      <img src="..." class="d-block w-100">
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
 </div>
-
         </main> 
     </body>
 </html>
