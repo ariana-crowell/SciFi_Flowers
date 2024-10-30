e1992b2389727e57b20fdc93defe6d16b23be445
ariana-crowell <crowella66@gmail.com>
2024-10-30 08:44:20 -0500

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -42,7 +42,13 @@
                     </div>
                 </div>
             </div>
-            
+            <div class="mini-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="copyright-text">
+              <p>© 2024. All rights reserved. Created by SciFi Flowers</p>
+            </div>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         </footer>
     </body>
