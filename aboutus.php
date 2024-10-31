fdcc1794f2bea67cc8c6f11c89e0e2764322698c
Kaitlyn-kodes <Kaitlynellis29@gmail.com>
2024-10-31 08:45:13 -0500

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -11,55 +11,65 @@
         .aboutus {
         font-family: Jacques Francois Shadow;
         color:whitesmoke;
-      }
-      .card{
-        margin:30px;
-      }
-      .container {
+        }
+        body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.flip-card {
+  background-color: transparent;
+  width: 300px;
+  height: 300px;
+  perspective: 1000px;
+}
+
+.flip-card-inner {
   position: relative;
-  width: 50%;
-}
-
-.image {
-  display: block;
   width: 100%;
-  height: auto;
-  
-}
-
-.overlay {
-  position: absolute;
-  bottom: 100%;
-  left: 0;
-  right: 0;
-  background-color: #008CBA;
-  overflow: hidden;
-  width: 100%;
-  height:0;
-  transition: .5s ease;
-}
-
-.container:hover .overlay {
-  bottom: 0;
   height: 100%;
-}
-
-.text {
-  color: white;
-  font-size: 20px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  -webkit-transform: translate(-50%, -50%);
-  -ms-transform: translate(-50%, -50%);
-  transform: translate(-50%, -50%);
   text-align: center;
-}
-.size{
-height:900px;
-width: 900px;
+  transition: transform 0.6s;
+  transform-style: preserve-3d;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
 }
 
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
+}
+
+.flip-card-front, .flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+}
+
+.flip-card-front {
+  background-color: #bbb;
+  color: black;
+}
+
+.flip-card-back {
+  background-color: #2980b9;
+  color: white;
+  transform: rotateY(180deg);
+}
+/* Float four columns side by side */
+.column {
+  float: left;
+  width: 25%;
+  padding: 0 10px;
+}
+/* Remove extra left and right margins, due to padding */
+.row {margin: 0 -5px;}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
     </style>
     </head>
     <body>
@@ -71,44 +81,87 @@ width: 900px;
     <center><h1 class="aboutus">About Us</h1></center>
         <br>
 
+        <div class="column">
+    <div class="card">
+    <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="img/First_scientist_better.jpg" alt="Avatar" style="width:300px;height:300px;">
+    </div>
+    <div class="flip-card-back">
+      <h1 class="aboutus" >Gina Hydrangea</h1> 
+      <ul>
+        <li><p>Professor at Cornell University</p> </li>
+        <li><p>Taught botany and biology</p>  </li>
+        <li><p>Loves to travel and discover exotic plants</p></li>
+        <li><p>Head of Sci-Fi Flowers Labatory</p></li>
+      </ul>  
+      <br>
+      <p>Drhydrangea@gmail.com</p>
+      <p>971-222-4444</p>
+      <p>526 Sammie Dr, Seaside, Oregon</p>   
+      
+    </div>
+  </div>
+</div>
+    </div>
+  </div>
 
-<div class="container " >
-  <img src="img/First_scientist_better.jpg" alt="Avatar" class="image ">
-  <div class="overlay">
-    <div class="text"> Dr. Hydrangea, a professor at Cornell University, taught botany and biology,
-         while also enjoying her passion of exploring the world. Multiple times a year
-          she travels to different locations in South America to discover plants to bring 
-          back to her class. Over the years, her love to travel to exotic locations to find
-           rare plants outgrew her passion of classroom teaching, so she decided to partner 
-           with someone who would be interested in working with these exotic plants. 
-           Still honoring her love for biology, she now leads our Labatory department and creates our artificial plants.</div>
+  <div class="column">
+    <div class="card">
+    <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="img/third_botanist.jpg" alt="Avatar" style="width:300px;height:300px;">
+    </div>
+    <div class="flip-card-back">
+      <h1 class="aboutus">Sapphire Johnson</h1> 
+      <ul>
+        <li><p>Botany major at Cornell University</p> </li>
+        <li><p>Immense love for botany and Science Fiction </p>  </li>
+        <li><p>Joined Sci-Fi Flowers with Dr. Hydrangea</p></li>        
+      </ul>  
+      <br>
+      <p>saphjohnson@gmail.com</p>
+      <p>971-444-2222</p>
+      <p>526 Sammie Dr, Seaside, Oregon</p>   
+      
+    </div>
   </div>
 </div>
-<div class="container size" >
-  <img src="img/third_botanist.jpg" alt="Avatar" class="image size">
-  <div class="overlay">
-    <div class="text"> Sapphire Johnson grew her passion for plants in a different light. 
-      For years she has loved to learn about flowers and is a botany major
-       at Cornell University. Her love for botany begins in her childhood as
-        she receives flowers from her father every week. These flowers 
-        intrigued her, but so did the flowers she read about in her SciFi Novels.
-         She then paired her passion for botany with science fiction and presented 
-         the idea of SciFi flowers to her professor and peer, Dr. Hydrangea. Together,
-          they partnered with Sapphire to bring the company SciFi Flowers to life.</div>
+    </div>
+  </div>
+       
+  <div class="column">
+    <div class="card">
+    <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="img/2nd_botanist_with_flowers.jpg" alt="Avatar" style="width:300px;height:300px;">
+    </div>
+    <div class="flip-card-back">
+      <h1 class="aboutus">Lucia Lobbern</h1> 
+      <ul>
+        <li><p>Obtained BBA at University of Oregon</p> </li>
+        <li><p>Immense love for botany and Science Fiction </p>  </li>
+        <li><p>Founded Sci-Fi Flowers in 2024</p></li>        
+      </ul>  
+      <br>
+      <p>lucialobbern@gmail.com</p>
+      <p>971-222-4422</p>
+      <p>526 Sammie Dr, Seaside, Oregon</p>   
+      
+    </div>
   </div>
 </div>
-<div class="container size" >
-  <img src="img/2nd_botanist_with_flowers.jpg" alt="Avatar" class="image size">
-  <div class="overlay">
-    <div class="text"> Lucia Lobbern began her passion for plants at a young age. 
-      Growing up in a small cottage in Oregon, she spent most afternoons 
-      exploring the world around her, and to her knowledge it was forestry. 
-      She would find strange plants of beautiful nature and test them. This 
-      curiosity led her to create a flower business in her hometown, selling 
-      traditional flowers, but she knew she wanted to reach a greater audience,
-       and have a flower shop that stands out from the rest.</div>
+    </div>
   </div>
-</div>
+
+
+
+
+
+
      
         <?php
             include 'includes/footer.php';
