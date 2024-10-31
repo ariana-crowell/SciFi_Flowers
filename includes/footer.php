89911c7802f09c08aafb9e07681709fdef55b0aa
ariana-crowell <crowella66@gmail.com>
2024-10-31 08:07:50 -0500

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -7,14 +7,11 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="footer_style.css">
         <style>
-            .footer {
-                position: relative;
-                left: 20px;
-                right: 20px;
-                bottom: 0;
-                background-color: rgba(117, 117, 117, 0.805);
-                color: white;
+            footer {
+                background-color: #777;
+                padding: 10px;
                 text-align: center;
+                color: white;
             }
 
             .footerlink {
@@ -23,33 +20,28 @@
                 text-decoration: none;
                 font-size: 30px;
             }
+            .copyright-text {
+                background-color: rgba(117, 117, 117, 0.805);
+                color: white;
+                text-align: center;
+            }
         </style>
     </head>
     <body>
         <footer>
-            <div class="container text-center footer">
-                <div class="row">
-                    <div class="col">
-                        <img class="footerpic" src="img/headlabcontact.png" alt="contact">
-                    </div>
-                    <div class="col">
-                        <a class="footerlink" href="termsandconditions.html">Terms and Conditions</a>
-                        <br>
-                        <a class="footerlink" href="purchaseandpaymentpolicy.html">Purchase & Payment Policy</a>
-                    </div>
-                    <div class="col">
-                        <img class="footerpic" src="img/ourofficecontact.png" alt="contact">
-                    </div>
-                </div>
+        <div class="text-center footer">
+            <div class="row">
+                <a class="footerlink" href="termsandconditions.html">Terms and Conditions</a>
+                <br>
+                <a class="footerlink" href="purchaseandpaymentpolicy.html">Purchase & Payment Policy</a>
             </div>
-            <div class="mini-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
+        </div>
+        <div class="mini-footer">
             <div class="copyright-text">
-              <p>© 2024. All rights reserved. Created by SciFi Flowers</p>
-            </div>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+                <p>© 2024. All rights reserved. Created by SciFi Flowers</p>
+        </div>
         </footer>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        
     </body>
 </html>
