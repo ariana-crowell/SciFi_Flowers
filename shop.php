d65bdfd12213a3622b8a6db613ee3ed25cba6362
ariana-crowell <crowella66@gmail.com>
2024-11-06 08:44:17 -0600

    Update shop.php

--- shop.php
+++ shop.php
@@ -7,42 +7,66 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="style.css">
         <link href="img/logo.png" rel="icon">
+        <style>
+            .card{
+                margin-top:5px;
+                margin-right: 25px;
+                margin-bottom:5px;
+                margin-left: 25px;
+            }
+        </style>
     </head>
     <body>
         <?php 
             require "includes/header.php"; 
         ?>
+        <br>
         <div class="row">
-            <div class="card col-sm-4" style="width: 18rem; background-color:gray;">
+            <div class="card col-sm-3" style="width: 18rem; background-color:gray;">
                 <img src="..." class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <div class="row">
-                        <a href="#" class="btn btn-primary col-sm-6">Checkout Now</a>
-                        <a href="#" class="btn btn-success col-sm-6">Add to Cart</a>
+                        <a href="#" class="btn btn-primary col-sm-5">Checkout Now</a>
+                        <p class="col-sm-2">     </p>
+                        <a href="#" class="btn btn-success col-sm-5">Add to Cart</a>
                     </div>
                 </div>
             </div>
-            <div class="card col-sm-4" style="width: 18rem; background-color:gray;">
+            <div class="card col-sm-3" style="width: 18rem; background-color:gray;">
                 <img src="..." class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <div class="row">
-                        <a href="#" class="btn btn-primary col-sm-6">Checkout Now</a>
-                        <a href="#" class="btn btn-success col-sm-6">Add to Cart</a>
+                        <a href="#" class="btn btn-primary col-sm-5">Checkout Now</a>
+                        <p class="col-sm-2">     </p>
+                        <a href="#" class="btn btn-success col-sm-5">Add to Cart</a>
                     </div>
                 </div>
             </div>
-            <div class="card col-sm-4" style="width: 18rem; background-color:gray;">
+            <div class="card col-sm-3" style="width: 18rem; background-color:gray;">
                 <img src="..." class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                     <div class="row">
-                        <a href="#" class="btn btn-primary col-sm-6">Checkout Now</a>
-                        <a href="#" class="btn btn-success col-sm-6">Add to Cart</a>
+                        <a href="#" class="btn btn-primary col-sm-5">Checkout Now</a>
+                        <p class="col-sm-2">     </p>
+                        <a href="#" class="btn btn-success col-sm-5">Add to Cart</a>
+                    </div>
+                </div>
+            </div>
+            <div class="card col-sm-3" style="width: 18rem; background-color:gray;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div class="row">
+                        <a href="#" class="btn btn-primary col-sm-5">Checkout Now</a>
+                        <p class="col-sm-2">     </p>
+                        <a href="#" class="btn btn-success col-sm-5">Add to Cart</a>
                     </div>
                 </div>
             </div>
